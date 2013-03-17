6542e08864e9e96e60fc1b576cad334341ade9df
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-03-17 15:47:22 +0100

    1.3.0 testing

--- include/head.php
+++ include/head.php
@@ -8,19 +8,19 @@
 
 <!-- Basic
 ================================================== -->
-<meta charset="utf-8">
+<meta charset="utf-8" />
 <title>nginZ</title>
-<meta name="description" content="EngineZ">
-<meta name="author" content="Zlatan Vasović">
-<meta name="keywords" content="zdroid, engine, z, enginez, net, nginz">
+<meta name="description" content="Engine~Z" />
+<meta name="author" content="Zlatan Vasović" />
+<meta name="keywords" content="nginz, web, gnu/linux, engine~z, unix" />
 
 <!-- Mobile
 ================================================== -->
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
 <!-- CSS
 ================================================== -->
-<link rel="stylesheet" href="styles/styles.css">
+<link rel="stylesheet" href="styles/styles.css" />
 
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -28,21 +28,22 @@
 
 <!-- Favicons
 ================================================== -->
-<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-<link rel="icon" href="img/favicon.ico" type="image/x-icon">
-<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
-<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
 </head>
 
 <!-- Page
 ================================================== -->
 <body>
-<div class="top"><strong>I'm top div class. I generated for notices, but you can use me for everything you want.</strong></div>
-<nav><ul>
-<li><a href="/">Home</a></li>
-<li><a href="tweet.php">Tweet</a></li>
-<li><a href="projects.php">Projects</a></li>
-<li><a href="about.php">About</a></li>
+<div class="top"><strong><marquee>I'm top div class. I generated for notices, but you can use me for everything you want.</marquee></strong></div>
+<nav><ul><!-- Navigation -->
+<li><strong><a href="./">Home</a></strong></li>
+<li><strong><a href="tweet.php">Tweet</a></strong></li>
+<li><strong><a href="projects.php">Projects</a></strong></li>
+<li><strong><a href="about.php">About</a></strong></li>
 </ul>
 </nav>
+<br /><br /><br />
--- projects.php
+++ projects.php
@@ -5,11 +5,11 @@ include 'include/head.php';
 
 <p><strong>Projects page</strong></p>
 
-<h2>Category 1</h2><hr><br />
+<h2><span style="color: #336699">@</span> Category 1</h2><hr><br />
 <p><strong>Project 1</strong> - description<br />
 <strong>Project 2</strong> - description<br />
 <strong>Project 3</strong> - description</p>
-<h2>Category 2</h2><hr><br />
+<h2><span style="color: #EB3333">#</span> Category 2</h2><hr><br />
 <p><strong>Project 1</strong> - description<br />
 <strong>Project 2</strong> - description<br />
 <strong>Project 3</strong> - description</p>
