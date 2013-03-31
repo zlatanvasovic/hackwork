1df9a5b37cd225d9c695699894c48f288ec81d9a
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-03-31 11:01:43 +0200

    Dropped IE6 support, minified numbet of fonts

--- extras/blog/index.php
+++ extras/blog/index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
 <!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
 <!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<head>
 
+<head>
 <!-- Basic
 ================================================== -->
 <meta charset="utf-8" />
---
+++ extras/blog/space/example.md
@@ -0,0 +1,10 @@
+**/extras/blog/space/** directory is used for storage of images and other binary data. You can also use it for preparing new blog posts.
+
+```
+                             __
+ ___ _ __   __ _  ___ ___   / /
+/ __| '_ \ / _` |/ __/ _ \ / /
+\__ \ |_) | (_| | (_|  __// /
+|___/ .__/ \__,_|\___\___/_/
+    |_|
+```
