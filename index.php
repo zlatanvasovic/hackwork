c8af33564588fe0844fc8f2a1d659f374183b8fd
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-04-09 10:07:20 +0200

    Fixed index.php, added travis conf

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'include/head.php';
 <h1><span style="color: orange;">ngin</span><span style="color: #6EB825;">Z</span></h1>
 
 <p>Web site engine</p>
-<p><a class="button" href="http://zdroid.anapnea.net">Web site</a> <a class="button" href="https://twitter.com/ZXeDroid">Twitter</a> <a class="button" href="https://github.com/ZDroid/nginZ/wiki">Wiki</a></p>
+<p><a class="btn" href="http://zdroid.anapnea.net">Web site</a> <a class="btn" href="https://twitter.com/ZXeDroid">Twitter</a> <a class="btn" href="https://github.com/ZDroid/nginZ/wiki">Wiki</a></p>
 <p><img src="img/wallpaper_05.jpg" width="635" height="369" alt="AW05" /></p>
 <?php
 include 'include/footer.php';
