ce446aa2c98de52df33617d0314e490b72473a90
ZDroid <legospace9876@gmail.com>
2013-07-31 15:48:44 +0200

    PHP fix, removed Travis

--- php/fnc.php
+++ php/fnc.php
@@ -1,10 +1,11 @@
 <?php
 
 //
-// NginZ functions
+// nginZ's functions
 //
 
 // Checks if the current is...
+// Requires styling of `.current` CSS class
 function curFile($file) {
 	if (strpos($_SERVER["PHP_SELF"], $file)) echo 'class="current"';
 }
