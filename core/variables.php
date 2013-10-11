90ecf74e018476daa5948a3ddc52554f85a20016
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-10-11 21:05:11 +0200

    Fix default variable values

--- core/variables.php
+++ core/variables.php
@@ -15,8 +15,8 @@ $title = 'Site';
 if (isset($slug)) {
   $title = "$slug &middot; $title";
 }
-$description = 'My personal site.';
-$author = 'Me';
+$description = "Personal site.";
+$author = 'Mr. Ghost';
 $keywords = 'site, website, web';
 $robots = 'noodp,noydir';
 $viewport = 'width=device-width, initial-scale=1.0';
