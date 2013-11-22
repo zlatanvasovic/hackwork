9deec5088a6d3a1df9160d2240ada483ea6dfe93
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-11-22 17:16:15 +0100

    Combine vars with strings in include paths

--- core/hackwork.php
+++ core/hackwork.php
@@ -14,12 +14,12 @@ define('PATH', ROOT);
 // `$layout` is a layout name, `$data` is a name of file which will be included
 // from `data/`.
 function layout($layout, $data) {
-  foreach (glob(PATH . '/layouts/' . $layout . '/*.php') as $item) {
+  foreach (glob(PATH . "/layouts/$layout/*.php") as $item) {
     if (preg_match('/set.*.php$/', $item)) {
       require_once($item);
     }
   }
-  include_once PATH . '/layouts/' . $layout . '/header.php';
-  include_once PATH . '/data/' . $data . '.php';
-  include_once PATH . '/layouts/' . $layout . '/footer.php';
+  include_once PATH . "/layouts/$layout/header.php";
+  include_once PATH . "/data/$data.php";
+  include_once PATH . "/layouts/$layout/footer.php";
 }
