19c8927d0bc2735432de56ae746dec9d30a62d12
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-12-15 18:53:52 +0100

    Add basic env switch

--- core/hackwork.php
+++ core/hackwork.php
@@ -1,24 +1,60 @@
 <?php
 
-/*
+/**
  * Hackwork
  *
  * Simple, layout-based PHP micro framework for making HTML5 sites.
  * http://git.io/hackwork
  */
 
-// Define paths
+/*
+ * Environment
+ *
+ * Valid values:
+ *   development
+ *   production
+ */
+
+define('ENVIRONMENT', 'production');
+
+// Define consistent error reporting settings
+switch (ENVIRONMENT) {
+  case 'development':
+    error_reporting(-1);
+    ini_set('display_errors', 1);
+  break;
+
+  case 'production':
+    error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    ini_set('display_errors', 0);
+  break;
+
+  default:
+    header('HTTP/1.1 503 Service Unavailable', TRUE, 503);
+    echo 'The application environment is incorrect.';
+    exit(1); // generic error
+}
+
+/*
+ * Define paths
+ *
+ * Remember, no trailing slashes!
+ */
+
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('PATH', ROOT);
 define('ASSETS', '/assets');
 define('DATA', PATH . '/data');
 define('LAYOUTS', PATH . '/layouts');
 
-// Generate layout
-//
-// `$layout`     => layout name
-// `$data`       => data file name
-// `$page_title` => [optional] page title
+/*
+ * Generate layout
+ *
+ * `$layout`     => layout name
+ * `$data`       => data file name
+ * `$page_title` => [optional] page title
+ */
+
 function layout($layout, $data, $page_title = false) {
   foreach (glob(PATH . "/layouts/$layout/*.php") as $item) {
     if (preg_match('/set.*.php$/', $item)) {
