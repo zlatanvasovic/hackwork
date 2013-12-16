8c05c714e25f402ebc52e169567e3de9eba06aa6
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-12-16 19:38:27 +0100

    Move layout() to separate file (helper)

--- core/hackwork.php
+++ core/hackwork.php
@@ -56,21 +56,3 @@ foreach (glob(PATH . '/core/*.php') as $helper) {
     require_once($helper);
   }
 }
-
-/*
- * Generate layout
- *
- * `$layout`     => layout name
- * `$data`       => data file name
- * `$page_title` => [optional] page title
- */
-
-function layout($layout, $data, $page_title = false) {
-  foreach (glob(PATH . "/layouts/$layout/set.*.php") as $item) {
-    require_once($item);
-  }
-
-  require_once LAYOUTS . "/$layout/header.php";
-  require_once DATA . "/$data.php";
-  require_once LAYOUTS . "/$layout/footer.php";
-}
